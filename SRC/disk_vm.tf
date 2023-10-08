99d7829b9da5ee41d87c11422d13abdb034e7429
Demonium <demoniumxxx@gmail.com>
2023-10-08 10:36:47 +0700

    Поправил переменные ресурсов в disk_vm.tf

--- SRC/disk_vm.tf
+++ SRC/disk_vm.tf
@@ -5,14 +5,26 @@ resource "yandex_compute_disk" "disks" {
   count = 3
   block_size  = 4096
 }
+variable "yandex_compute_instance_storage" {
+  type = map(map(number))
+  default = {
+    storage_resources = {
+      cores = 2
+      memory = 2
+      core_fraction = 5
+    }
+  }
+}
+
 
 resource "yandex_compute_instance" "storage" {
   name = "storage"
   zone = "ru-central1-a"
 
   resources {
-    cores  = 2
-    memory = 2
+    cores  = var.yandex_compute_instance_storage.storage_resources.cores
+    memory = var.yandex_compute_instance_storage.storage_resources.memory
+    core_fraction = var.yandex_compute_instance_storage.storage_resources.core_fraction
   }
 
   boot_disk {
@@ -40,6 +52,33 @@ resource "yandex_compute_instance" "storage" {
   }
   }
 }
+resource "null_resource" "web_hosts_provision" {
+  #Ждем создания инстанса
+  depends_on = [yandex_compute_instance.storage]
+
+  #Добавление ПРИВАТНОГО ssh ключа в ssh-agent
+  provisioner "local-exec" {
+    command = "cat ~/.ssh/id_rsa | ssh-add -"
+  }
+  #Костыль!!! Даем ВМ 60 сек на первый запуск. Лучше выполнить это через wait_for port 22 на стороне ansible
+# В случае использования cloud-init может потребоваться еще больше времени
+ provisioner "local-exec" {
+    command = "sleep 120"
+  }
+
+#Запуск ansible-playbook
+  provisioner "local-exec" {
+    command  = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${abspath(path.module)}/hosts.cfg ${abspath(path.module)}/test.yml"
+    on_failure = continue #Продолжить выполнение terraform pipeline в случае ошибок
+    environment = { ANSIBLE_HOST_KEY_CHECKING = "False" }
+    #срабатывание триггера при изменении переменных
+  }
+    triggers = {
+      always_run         = "${timestamp()}" #всегда т.к. дата и время постоянно изменяются
+      playbook_src_hash  = file("${abspath(path.module)}/test.yml") # при изменении содержимого playbook файла
+      ssh_public_key     = local.ssh-keys # при изменении переменной
+    }
+  }
 resource "null_resource" "web_hosts_provision" {
   #Ждем создания инстанса
   depends_on = [yandex_compute_instance.storage]
